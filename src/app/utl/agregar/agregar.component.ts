13d3b5ba7ae0c77b62ab8f28b19378fee0ca7b05
isaacpanchito <isaacemj.26@gmail.com>
2022-10-20 13:37:35 -0500

    padre e hijo

---
+++ src/app/utl/agregar/agregar.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+import {AlumnosUtl} from '../interfaces/utl.interface';
+import { UtlService } from '../service/utl.service';
+import { UtlModule } from '../utl.module';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styleUrls: ['./agregar.component.css']
+})
+export class AgregarComponent{
+
+
+  /* @Input() alumnosUtl:AlumnosUtl[]=[]; */
+  @Input() regAlum:AlumnosUtl={
+    nombre:'',
+    edad:0
+  }
+
+  @Output()onNuevoAlumnos:EventEmitter<AlumnosUtl>= new EventEmitter();
+
+  constructor(private UtlService:UtlService){}
+  
+  agregar(){
+    //console.log(this.regAlumno);
+   /*  this.alumnosUtl.push(this.regAlum); */
+   //this.UtlService.mostrarSaludo();
+    this.onNuevoAlumnos.emit(this.regAlum);
+    this.regAlum={
+      nombre:'',
+      edad:0
+    }
+  }
+}
--- src/app/utl/main-page/main-page.component.ts
+++ src/app/utl/main-page/main-page.component.ts
@@ -1,48 +1,29 @@
-import { Component} from '@angular/core';
-interface AlumnosUtl{
-  nombre:string;
-  edad:number;
-}
+import { Component } from '@angular/core';
+import { AlumnosUtl } from '../interfaces/utl.interface';
+import { UtlService } from '../service/utl.service';
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
-            /* <li>lucia - 22</li>
+  /* <li>lucia - 22</li>
             <li>Leonardo -21</li>
             <li>Diana -21</li> */
-alumnosUtl:AlumnosUtl[]=[
-  {
-    nombre:'Mario',
-    edad:15
-  },
-  {
-    nombre:'Lucia',
-    edad:22
-  },
-  {
-    nombre:'Leonardo',
-    edad:21
-  },
-  {
-    nombre:'Diana',
-    edad:21
-  }
 
-]
-regAlumno:AlumnosUtl={
-  nombre:'',
-  edad:0
-}
-agregar(){
-  //console.log(this.regAlumno);
-  this.alumnosUtl.push(this.regAlumno);
-  this.regAlumno={
-    nombre:'',
-    edad:0
-  }
-}
-  
+  regAlum: AlumnosUtl = {
+    nombre: '',
+    edad: 0,
+  };
+  constructor(private utlService:UtlService){}
+
+  /*agregarNuevoAlumnodata:alumnosUtl){
+    this.alumnosUtl.push(datos);
+    this.
+    UtlService.mostrarSaludo();
+  }*/
+
+
 
 }
